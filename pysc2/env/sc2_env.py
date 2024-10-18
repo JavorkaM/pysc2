f4eef9b88fba090f80700e8fcbe81d88ab5b428f
Marcell Javorka <ma.javorka@gmail.com>
2024-10-18 09:06:18 +0200

    Initial Starter bot

--- pysc2/env/sc2_env.py
+++ pysc2/env/sc2_env.py
@@ -335,7 +335,7 @@ class SC2Env(environment.Base):
 
     # Actually launch the game processes.
     self._sc2_procs = [
-        self._run_config.start(extra_ports=self._ports,
+        self._run_config.start(
                                want_rgb=interface.HasField("render"))
         for interface in self._interface_options]
     self._controllers = [p.controller for p in self._sc2_procs]
